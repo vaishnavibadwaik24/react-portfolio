0e0fb12be2673d3c94192da8a55ca90cf5bd1fd8
Vaishnavi A Badwaik <88033256+vaishnavibadwaik24@users.noreply.github.com>
2024-05-26 17:41:13 +0530

    header completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Portfolio from './Portfolio';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Portfolio />
   </React.StrictMode>
 );
 
